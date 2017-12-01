505bb72f7f2d7c0c725184d1d86e69950712d488
nmivan <nmivan@mail.ru>
2017-12-01 23:59:51 +0500

    Переделал вычисляемые свойства характеристики на reduce

--- src/metadata/catalogs/cat_characteristics.js
+++ src/metadata/catalogs/cat_characteristics.js
@@ -17,21 +17,34 @@ export default function ($p) {
         //Фурнитура
         const furns = $p.wsql.alasql("select first(furn) as furn from ? where furn <> ?", [this.constructions._obj, $p.utils.blank.guid]);
 
-        const nom = this.specification.unload_column("nom");
-        //признаки нестандартов
-        const non_standard = $p.wsql.alasql("select sum(crooked) as crooked, sum(colored) as colored, sum(lay) as lay, sum(made_to_order) as made_to_order, sum(packing) as packing from ?", [nom]);
-        const days_to_execution = $p.wsql.alasql("select min(days_to_execution) as days_to_execution from ?", [nom]);
-
-        return {
+        const res = {
           sys: this.sys,
           furn: (furns.length == 0 ? $p.utils.blank : $p.cat.furns.get(furns[0].furn)),
-          crooked: non_standard[0].crooked > 0,
-          colored: non_standard[0].colored > 0,
-          lay: non_standard[0].lay > 0,
-          made_to_order: non_standard[0].made_to_order > 0,
-          packing: non_standard[0].packing > 0,
-          days_to_execution: days_to_execution[0].days_to_execution
-        }
+          crooked: false,
+          colored: false,
+          lay: false,
+          made_to_order: false,
+          packing: false,
+          days_to_execution: 0
+        };
+
+        const specification = this.specification._obj;
+
+        specification.reduce((res, row)=>{
+          const nom = $p.cat.nom.get(row.nom);
+
+          res.crooked = res.crooked || !!nom.crooked;
+          res.colored = res.colored || !!nom.colored;
+          res.lay = res.lay || !!nom.lay;
+          res.made_to_order = res.made_to_order || !!nom.made_to_order;
+          res.packing = res.packing || !!nom.packing;
+
+          res.days_to_execution = Math.max(res.days_to_execution, nom.days_to_execution);
+
+          return res;
+        }, res)
+
+        return res;
       }
     }
     }
