0e35d41a6e736349106947f318b2100722078510
unpete <info@oknosoft.ru>
2023-04-04 23:42:38 +0500

    регистрация заказов с сохранением исходного отдела

--- server/keys/subscription/reflect.js
+++ server/keys/subscription/reflect.js
@@ -1,16 +1,61 @@
 
-module.exports = function ($p, log, accumulation) {
-  const {utils: {sleep}, doc: {calc_order}} = $p;
 
-  async function order(doc) {
+module.exports = function ($p, log, acc) {
+  const {utils: {sleep, blank}, cat: {branches}, doc: {calc_order}} = $p;
+
+  async function order({doc, branch, abonent, year}) {
+    const {ref, date, partner, organization, manager, department, number_doc} = doc;
+    const {rowCount, rows} = await acc.client.query(`SELECT branch from calc_orders WHERE ref=$1`, [ref]);
+    const values = [
+      ref,
+      abonent.valueOf(),
+      branch.valueOf(),
+      year,
+      date,
+      partner.valueOf(),
+      organization.valueOf(),
+      manager.valueOf(),
+      department.valueOf(),
+      number_doc
+    ];
+    if(rowCount) {
+      const tb = branches.get(rows[0].branch);
+      if(branch.empty() && !tb.empty()) {
+        values[2] = tb.valueOf();
+      }
+      else if(!branch.empty() && tb.empty()) {
+        values[2] = branch.valueOf();
+      }
+      else if(!branch.empty() && !tb.empty()) {
+        if(branch._hierarchy(tb)) {
+          values[2] = branch.valueOf();
+        }
+        else if(tb._hierarchy(branch)) {
+          values[2] = tb.valueOf();
+        }
+      }
+      return acc.client.query(`UPDATE calc_orders SET
+            abonent = $2,
+            branch = $3,
+            year = $4,
+            date = $5,
+            partner = $6,
+            organization = $7,
+            author = $8,
+            department = $9,
+            number_doc = $10
+            WHERE ref=$1;`, values);
+    }
+    return acc.client.query(`INSERT INTO calc_orders
+            (ref, abonent, branch, year, date, partner, organization, author, department, number_doc)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, values);
+  }
+
+  async function cx({ox, branch, abonent, year}) {
 
   }
 
-  async function cx(ox) {
-
-  }
-
-  async function keys(doc, ox) {
+  async function keys({doc, ox, branch, abonent, year}) {
     if(ox) {
 
     }
@@ -27,14 +72,14 @@ module.exports = function ($p, log, accumulation) {
       const doc = calc_order.create(attr, false, true);
       const prod = await doc.load_production(false, db);
       // запись в таблице calc_orders
-      await order(doc);
+      await order({doc, branch, abonent, year});
       // запись в таблице keys документа Расчёт
-      await keys(doc);
+      await keys({doc, branch, abonent, year});
       for(const ox of prod) {
         // запись в таблице characteristics
-        await cx(ox);
+        await cx({ox, branch, abonent, year});
         // запись в таблице keys ключей продукции
-        await keys(doc, ox);
+        await keys({ox, branch, abonent, year});
       }
       for(const ox of prod) {
         ox.unload();
