79265167ddefc8175bf0606ccd3018c09f202c4e
unpete <info@oknosoft.ru>
2023-09-13 12:07:49 +0500

    уточнение в reflect

--- server/keys/subscription/reflect.js
+++ server/keys/subscription/reflect.js
@@ -201,7 +201,7 @@ module.exports = function ($p, log, acc) {
       const {_id, _rev, ...attr} = result.doc;
       attr.ref = _id.substring(15);
       if(attr.ref.length !== 36) {
-        return log(new Error(`Ошибка формата uid\nobj=${attr.ref
+        log(new Error(`Ошибка формата uid\nobj=${attr.ref
         }\nbranch='${branch.suffix}' ${branch.valueOf()
         }\nabonent=${abonent.id}`));
         continue;
