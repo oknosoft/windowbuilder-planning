2c82ecf2a64065fbb4dfc1af45ca966e6007e766
unpete <info@oknosoft.ru>
2024-03-25 19:38:40 +0500

    уточнение запроса

--- server/planing/get.js
+++ server/planing/get.js
@@ -11,10 +11,10 @@ module.exports = function ($p, log, glob) {
   const {utils: {end, moment}, adapters, cat} = $p;
 
   const sql = `select rem.*, keys.ref, keys.obj, keys.specimen, keys.elm, keys.type  from
-  (select phase, date, shift, work_center, planing_key barcode, stage, calc_order, sum(sign * power) power  FROM areg_dates
+  (select date, shift, work_center, planing_key barcode, stage, calc_order, sum(sign * power) power  FROM areg_dates
   where phase = $1 and date between $2 and $3
-  GROUP BY phase, date, shift, work_center, barcode, stage, calc_order) rem
-  left outer join keys on rem.barcode = keys.barcode`;
+  GROUP BY date, shift, work_center, barcode, stage, calc_order) rem
+  left outer join keys on rem.barcode = keys.barcode order by date`;
 
   async function reminder(query = {}) {
     const pq = await glob.client.query(sql, [
