357c00eff9ac4d2033c10678e30b18f9a086db2a
unpete <info@oknosoft.ru>
2024-12-15 17:03:28 +0500

    уточнение в pgFeed

--- server/keys/feed.js
+++ server/keys/feed.js
@@ -16,7 +16,7 @@ function pgFeed($p, log, acc) {
       const pq = await acc.client.query(query.include_docs === 'true' ?
           `select case when characteristics.ref  is null then keys.obj else characteristics.calc_order end calc_order, feed.seq, keys.*
 from feed inner join keys on feed.ref = keys.ref
-left outer join characteristics on characteristics.ref = keys.obj where $1 limit $2` :
+left outer join characteristics on characteristics.ref = keys.obj where seq > $1 limit $2` :
           `select * from feed where seq > $1 limit $2`, [query.since || 0, query.limit || 100]);
       const last_seq = pq.rowCount ? pq.rows[pq.rowCount - 1].seq : query.since || 0;
       if(pq.rowCount) {
