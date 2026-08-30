9dabdd18592eb6ef46a4ca4ca05d0c39645a4c66
unpete <info@oknosoft.ru>
2026-08-30 17:30:52 +0300

    reflect_order

---
+++ server/planing/documents/reflect_order.js
@@ -0,0 +1,11 @@
+
+module.exports = function({client, utils, orders}) {
+  if(orders) {
+    orders.set(calc_order, 0);
+  }
+  if (doc.class_name = 'doc.work_centers_task') {
+    for(const {calc_order} of doc.set) {
+      orders.set(calc_order, 0);
+    }
+  }
+}
